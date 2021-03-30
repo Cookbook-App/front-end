ea21fe703f53272bc25480e69f96592eb0cc6fb4
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-03-29 18:35:02 -0600

    adding action

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,8 @@ export const FETCH_START = 'FETCH_START';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAILURE = 'FETCH_FAILURE';
 
+export const ADD_START = 'ADD_START'
+
 const recipeToFilter = []
 
 // Fetching actions
@@ -21,3 +23,15 @@ export const fetchCard = () => dispatch => {
     })
 }
 
+export const addRecipe = (index) => dispatch => {
+    dispatch({ type: ADD_START });
+    axiosWithAuth()
+    .post('/recipes', index)
+    .then(res => {
+        dispatch({ type: FETCH_SUCCESS, payload: res.data })
+        return true
+    })
+    .catch(err => {
+        dispatch({ type: FETCH_FAILURE })
+    })
+}
